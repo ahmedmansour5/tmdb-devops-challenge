8110c2c5c4267c1c1edee5a0f245f716bd0a90ff
Ahmed Mansour <ahmed_mansour_95@outlook.com>
2024-05-15 00:00:34 +0300

    Feat: adds terraform code for task 2

---
+++ tmdb-IaC/OCI/README.md
@@ -0,0 +1,3 @@
+# Jenkins-CI-CD-TMDB
+
+I have worked previously on this project to deploy a Jenkins Server on OCI using terraform. So this will be used to create the Jenkins Server in **Task 1**
---
+++ tmdb-IaC/gcp/tmdb-infra/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+}
+
+variable "region" {
+}
+
+variable "zone" {
+}
+
+variable "tmdb_ssh_user" {
+  default = "tmdb"
+}
+
+variable "tmdb_ssh_pub_key_file" {
+  default = "../../../tmdb-keys/tmdb-monitoring.pub"
+}
+
+variable "tmdb_ssh_private_key_file" {
+  default = "../../../tmdb-keys/tmdb-monitoring.key"
+}
+
