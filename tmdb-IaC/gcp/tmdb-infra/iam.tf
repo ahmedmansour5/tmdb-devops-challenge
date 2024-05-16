7aa7aa17f6845e5768d7dce434f9c9ebf8653704
Ahmed Mansour <ahmed_mansour_95@outlook.com>
2024-05-17 01:33:29 +0300

    Fix: add editor role for the account used to check my solution

--- tmdb-IaC/gcp/tmdb-infra/iam.tf
+++ tmdb-IaC/gcp/tmdb-infra/iam.tf
@@ -1,7 +1,7 @@
 # Creates a user to be used for checking the cloud resources
 
-resource "google_project_iam_member" "tmdb_viewer" {
+resource "google_project_iam_member" "tmdb_editor" {
   project = var.project_id
-  role    = "roles/viewer"
+  role    = "roles/editor"
   member  = "user:ahmedmansourua95@gmail.com"
 }
